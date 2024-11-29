01c221e9076b64f9519020a92646e6dc8f014a6e
LazyKitty <wenzhao5105@163.com>
2024-11-29 17:21:26 +0800

    fix bug

--- src/main.py
+++ src/main.py
@@ -5,6 +5,10 @@ from get_weather import get_weather_data
 from send_template import send_wechat_template_message
 from envConfig import envConfig
 
+# 发送天气消息
+def job_send_weather():
+    send_wechat_template_message(get_weather_data(), "")
+
 if __name__ == "__main__":
     # logging 加入时间戳并允许输出 INFO 级别的日志
     logging.basicConfig(
@@ -15,10 +19,7 @@ if __name__ == "__main__":
     scheduler = BlockingScheduler(timezone=timezone(envConfig.timezone))
     
     # 添加每日 08:00 执行报天气任务
-    scheduler.add_job(
-        send_wechat_template_message, 'cron', 
-        args=[get_weather_data, ""], hour=8, minute=0
-    )
+    scheduler.add_job(job_send_weather, 'cron', hour=8, minute=0)
     
     logging.info("Start")
     scheduler.start()
